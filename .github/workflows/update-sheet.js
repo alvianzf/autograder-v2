627022e8820b0b063efb5880c34ee2bfe87152c7
alvianzf <alvianzf@gmail.com>
2023-05-14 21:27:50 +0700

    console.log(env)

--- .github/workflows/update-sheet.js
+++ .github/workflows/update-sheet.js
@@ -4,21 +4,21 @@ require('dotenv').config();
 
 // console.log(process.env)
 
-async function updateSheet() {
-  const doc = new GoogleSpreadsheet('<SPREADSHEET_ID>');
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SHEETS_EMAIL,
-    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  });
-  await doc.loadInfo();
-  const sheet = doc.sheetsById['1S_51UI9RW3s5rB13TXlppAjabUDZouGM4ERFz_e-1v4'];
-  const row = await sheet.addRow({
-    Username: process.env.GITHUB_ACTOR,
-    Email: process.env.GITHUB_EMAIL,
-    'Successful Test Cases': parseInt(process.env.TEST_CASES_PASSED),
-    'Date and Time (GMT+7)': moment().tz('Asia/Jakarta').format(),
-  });
-  console.log(`Added row ${row.rowNumber} to the sheet.`);
-}
+// async function updateSheet() {
+//   const doc = new GoogleSpreadsheet('<SPREADSHEET_ID>');
+//   await doc.useServiceAccountAuth({
+//     client_email: process.env.GOOGLE_SHEETS_EMAIL,
+//     private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//   });
+//   await doc.loadInfo();
+//   const sheet = doc.sheetsById['1S_51UI9RW3s5rB13TXlppAjabUDZouGM4ERFz_e-1v4'];
+//   const row = await sheet.addRow({
+//     Username: process.env.GITHUB_ACTOR,
+//     Email: process.env.GITHUB_EMAIL,
+//     'Successful Test Cases': parseInt(process.env.TEST_CASES_PASSED),
+//     'Date and Time (GMT+7)': moment().tz('Asia/Jakarta').format(),
+//   });
+//   console.log(`Added row ${row.rowNumber} to the sheet.`);
+// }
 
-updateSheet();
+// updateSheet();
